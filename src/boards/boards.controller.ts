69b3e64bc21b231f267fb38d3d6a1a1f78379269
bobby <rlatnguscjsw@gmail.com>
2023-12-01 01:30:19 +0900

    Feat : Nest First commit
    - Nest JS Mygration First Commit

---
+++ src/boards/boards.controller.ts
@@ -0,0 +1,19 @@
+import { Board } from "./boards.model";
+import { BoardsService } from "./boards.service";
+import { Body, Controller, Get, Post } from "@nestjs/common";
+import { CreateBoardDto } from "./dto/create-board.dto";
+
+@Controller("boards")
+export class BoardsController {
+  constructor(private boardsService: BoardsService) {}
+
+  @Get()
+  getAllBoard(): Board[] {
+    return this.boardsService.getAllBoard();
+  }
+
+  @Post()
+  createBoard(@Body() createBoardDto: CreateBoardDto): Board {
+    return this.boardsService.createBoard(createBoardDto);
+  }
+}
---
+++ src/boards/boards.service.ts
@@ -0,0 +1,28 @@
+import { CreateBoardDto } from "./dto/create-board.dto";
+import { Injectable } from "@nestjs/common";
+import { Board, BoardStatus } from "./boards.model";
+import { v1 as uuid } from "uuid";
+
+@Injectable()
+export class BoardsService {
+  private board: Board[] = [];
+
+  getAllBoard(): Board[] {
+    return this.board;
+  }
+
+  createBoard(createBoardDto: CreateBoardDto): Board {
+    const { title, description } = createBoardDto;
+
+    const board: Board = {
+      id: uuid(),
+      title: title,
+      description: description,
+      status: BoardStatus.PUBLIC,
+    };
+
+    this.board.push(board);
+
+    return board;
+  }
+}
