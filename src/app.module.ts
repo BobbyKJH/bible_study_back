38cb767de5d7ae278c5ff0092da15083d4caee89
bobby <rlatnguscjsw@gmail.com>
2023-12-05 00:00:55 +0900

    Feat : AppModule
    - Data Base 연결

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,10 @@
 import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
 import { BoardsModule } from "./boards/boards.module";
+import { typeORMConfig } from "./config/typeorm.config";
+import { PbsModule } from './pbs/pbs.module';
 
-@Module({ imports: [BoardsModule] })
+@Module({
+  imports: [TypeOrmModule.forRoot(typeORMConfig), BoardsModule, PbsModule],
+})
 export class AppModule {}
