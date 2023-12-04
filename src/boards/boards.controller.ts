b756856e9bcf615bfe7f7334ffb7d833e51ca5c4
bobby <rlatnguscjsw@gmail.com>
2023-12-04 23:57:52 +0900

    Feat : DataBase를 이용한 Board Controller
    - DataBase를 사용 하여 Board 작성

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,32 +1,8 @@
-import { Body, Controller, Delete, Get, Param, Post } from "@nestjs/common";
+import { Controller } from "@nestjs/common";
 /** Service */
 import { BoardsService } from "./boards.service";
-/** Model */
-import { Board } from "./boards.model";
-/** Dto */
-import { CreateBoardDto } from "./dto/create-board.dto";
 
 @Controller("boards")
 export class BoardsController {
   constructor(private boardsService: BoardsService) {}
-
-  @Get()
-  getAllBoard(): Board[] {
-    return this.boardsService.getAllBoard();
-  }
-
-  @Get(":id")
-  getBoardById(@Param("id") id: string): Board {
-    return this.boardsService.getBoardById(id);
-  }
-
-  @Post()
-  createBoard(@Body() createBoardDto: CreateBoardDto): Board {
-    return this.boardsService.createBoard(createBoardDto);
-  }
-
-  @Delete(":id")
-  deleteBoard(@Param("id") id: string): void {
-    this.boardsService.deleteBoard(id);
-  }
 }
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,36 +1,4 @@
-import { CreateBoardDto } from "./dto/create-board.dto";
 import { Injectable } from "@nestjs/common";
-import { Board, BoardStatus } from "./boards.model";
-import { v1 as uuid } from "uuid";
 
 @Injectable()
-export class BoardsService {
-  private board: Board[] = [];
-
-  getAllBoard(): Board[] {
-    return this.board;
-  }
-
-  createBoard(createBoardDto: CreateBoardDto): Board {
-    const { title, description } = createBoardDto;
-
-    const board: Board = {
-      id: uuid(),
-      title: title,
-      description: description,
-      status: BoardStatus.PUBLIC,
-    };
-
-    this.board.push(board);
-
-    return board;
-  }
-
-  getBoardById(id: string): Board {
-    return this.board.find((board) => board.id === id);
-  }
-
-  deleteBoard(id: string): void {
-    this.board.filter((board) => board.id !== id);
-  }
-}
+export class BoardsService {}
