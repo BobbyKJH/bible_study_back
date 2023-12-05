d8b2871c42c7f6af555fc1d462c45bae399029f2
bobby <rlatnguscjsw@gmail.com>
2023-12-06 00:00:13 +0900

    Feat : Post CRUD
    - Post DataBase Entity 생성
    - Post C R U D 기능 추가

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,7 +1,56 @@
-import { Controller } from '@nestjs/common';
-import { PostsService } from './posts.service';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Put,
+} from "@nestjs/common";
+/** Service */
+import { PostsService } from "./posts.service";
 
-@Controller('posts')
+@Controller("posts")
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
+
+  /** Get /posts 모든 정보 가져온다 */
+  @Get()
+  getPosts() {
+    return this.postsService.getAllPosts();
+  }
+
+  /** Get /posts/:id 상세 정보 */
+  @Get(":id")
+  getPost(@Param("id") id: string) {
+    return this.postsService.getPostById(+id);
+  }
+
+  /** Post /posts 생성 */
+  @Post()
+  createPost(
+    @Body("author") author: string,
+    @Body("title") title: string,
+    @Body("content") content: string,
+  ) {
+    return this.postsService.createPost(author, title, content);
+  }
+
+  /** Put /posts/:id 수정 */
+  @Put(":id")
+  updatePost(
+    @Param("id") id: string,
+    @Body("author") author?: string,
+    @Body("title") title?: string,
+    @Body("content") content?: string,
+  ) {
+    return this.postsService.updatePost(+id, author, title, content);
+  }
+
+  /** Delete /posts/:id 삭제 */
+  @Delete(":id")
+  deletePost(@Param("id") id: string) {
+    return this.postsService.deletePost(+id);
+  }
 }
--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,4 +1,94 @@
-import { Injectable } from '@nestjs/common';
+import { InjectRepository } from "@nestjs/typeorm";
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { Repository } from "typeorm";
+/** Entity */
+import { PostsModel } from "./entities/posts.entity";
+
+export interface PostModel {
+  id: number;
+  author: string;
+  title: string;
+  content: string;
+  likeCount: number;
+  commentCount: number;
+}
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+  constructor(
+    @InjectRepository(PostsModel)
+    private readonly postsRepository: Repository<PostsModel>,
+  ) {}
+
+  async getAllPosts() {
+    return this.postsRepository.find();
+  }
+
+  getPostById(id: number) {
+    const found = this.postsRepository.findOne({ where: { id: id } });
+
+    if (!found)
+      throw new NotFoundException(
+        `해당하는 ${id}값을 게시물을 찾을 수 없습니다.`,
+      );
+
+    return this.postsRepository.findOne({ where: { id: id } });
+  }
+
+  createPost(author: string, title: string, content: string) {
+    const createPost = this.postsRepository.create({
+      author: author,
+      title: title,
+      content: content,
+      likeCount: 0,
+      commentCount: 0,
+    });
+
+    const newPost = this.postsRepository.save(createPost);
+
+    return newPost;
+  }
+
+  async updatePost(
+    postId: number,
+    author: string,
+    title: string,
+    content: string,
+  ) {
+    /** Save 기능
+     * 1) 만약 데이터가 존재하지 않는다면 (id) 새로 생성
+     * 2) 데이터가 존재한다면 존재하던 값을 업데이트 한다.
+     */
+    const post = await this.postsRepository.findOne({ where: { id: postId } });
+
+    if (!post) {
+      throw new NotFoundException(`${postId}는 없습니다.`);
+    }
+
+    if (author) {
+      post.author = author;
+    }
+    if (title) {
+      post.title = title;
+    }
+    if (content) {
+      post.content = content;
+    }
+
+    const newPost = await this.postsRepository.save(post);
+
+    return newPost;
+  }
+
+  async deletePost(postId: number) {
+    const post = await this.postsRepository.findOne({ where: { id: postId } });
+
+    if (!post) {
+      throw new NotFoundException(`${postId}는 없습니다.`);
+    }
+
+    await this.postsRepository.delete(postId);
+
+    return postId;
+  }
+}
