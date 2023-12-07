ca66836f3e65261efbb83fb777fd71d207fb9bd4
bobby <rlatnguscjsw@gmail.com>
2023-12-08 00:04:45 +0900

    Feat : Pbs Entity 생성
    - Pbs Entity Table 생성

--- src/pbs/entities/pbs.entity.ts
+++ src/pbs/entities/pbs.entity.ts
@@ -1,1 +1,47 @@
-export class Pbs {}
+import {
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from "typeorm";
+
+enum ShowData {
+  PUBLIC = "Y",
+  PRIMARY = "N",
+}
+
+@Entity()
+export class Pbs {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  book: string;
+
+  @Column()
+  chapter: number;
+
+  @Column()
+  startVerse: number;
+
+  @Column()
+  endVerse: string;
+
+  @Column("text")
+  content: string;
+
+  @Column({ type: "char", length: 1, default: ShowData.PUBLIC })
+  showData: ShowData;
+
+  @UpdateDateColumn({ type: "date" })
+  createAt: Date;
+
+  @Column()
+  view: number;
+
+  @Column()
+  userId: string;
+
+  @Column()
+  userName: string;
+}
