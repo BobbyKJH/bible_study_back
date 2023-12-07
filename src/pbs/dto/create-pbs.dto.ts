7eb83661eecf5c96113535804e290f07ea53614b
bobby <rlatnguscjsw@gmail.com>
2023-12-08 00:03:54 +0900

    Feat : Create Pbs Dto
    - Pbs 게시판 생성 Dto Create

--- src/pbs/dto/create-pbs.dto.ts
+++ src/pbs/dto/create-pbs.dto.ts
@@ -1,1 +1,22 @@
-export class CreatePbsDto {}
+enum ShowData {
+  PRIMARY = "N",
+  PUBLIC = "Y",
+}
+
+export class CreatePbsDto {
+  id: number;
+
+  book: string;
+
+  chapter: number;
+
+  startVerse: number;
+
+  endVerse: number;
+
+  content: string;
+
+  showData: ShowData;
+
+  view: 0;
+}
