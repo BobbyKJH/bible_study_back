ba9937c014638e50afb80d4717180ef1f0f5da5d
bobby <rlatnguscjsw@gmail.com>
2023-12-08 14:39:24 +0900

    Feat : User.module
    - User Entity 사용할 수 있도록 설정

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,14 @@
-import { Module } from '@nestjs/common';
-import { UsersService } from './users.service';
-import { UsersController } from './users.controller';
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+/** Controller */
+import { UsersController } from "./users.controller";
+/** Service */
+import { UsersService } from "./users.service";
+/** Entity */
+import { UsersModel } from "./entities/users.entity";
 
 @Module({
+  imports: [TypeOrmModule.forFeature([UsersModel])],
   controllers: [UsersController],
   providers: [UsersService],
 })
