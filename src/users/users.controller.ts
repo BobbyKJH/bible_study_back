44c4abc5ccd74bc9936825bb05ad37e526ee20f6
bobby <rlatnguscjsw@gmail.com>
2023-12-10 00:12:30 +0900

    Feat : User Controller
    - User Create Api 생성

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,26 @@
-import { Controller } from '@nestjs/common';
-import { UsersService } from './users.service';
+import { Body, Controller, Post } from "@nestjs/common";
+import { UsersService } from "./users.service";
 
-@Controller('users')
+@Controller("users")
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  /** Token */
+  @Post()
+  async createUser(
+    @Body("userId") userId: string,
+    @Body("userName") userName: string,
+    @Body("userEmail") userEmail: string,
+  ) {
+    const register = await this.usersService.createUser(
+      userId,
+      userName,
+      userEmail,
+    );
+
+    return register;
+  }
+
+  @Post("userId")
+  async loginUser(@Body("userId") userId: string) {}
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { UsersModel } from "./entities/users.entity";
+import { Repository } from "typeorm";
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(UsersModel)
+    private readonly usersRepository: Repository<UsersModel>,
+  ) {}
+
+  async createUser(userId: string, userName: string, userEmail: string) {
+    const user = this.usersRepository.create({
+      userId: userId,
+      userName: userName,
+      userEmail: userEmail,
+    });
+
+    const newUser = await this.usersRepository.save(user);
+
+    return newUser;
+  }
+
+  async loginUser(userId: string) {
+    this.usersRepository.findOne({
+      where: {
+        userId: userId,
+      },
+    });
+
+    return;
+  }
+}
