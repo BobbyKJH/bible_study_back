32e59d176a60080fba79384b37870a2030d15be1
bobby <rlatnguscjsw@gmail.com>
2023-12-12 00:56:51 +0900

    Feat : Qt Entity
    - Qt Entity Create

--- src/qt/entities/qt.entity.ts
+++ src/qt/entities/qt.entity.ts
@@ -1,1 +1,39 @@
-export class Qt {}
+import { Column, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+/** Enum */
+import { ShowData } from "src/pbs/const/pbs.const";
+
+@Entity()
+export class Qt {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  book: string;
+
+  @Column()
+  chapter: number;
+
+  @Column()
+  startVerse: number;
+
+  @Column()
+  endVerse: number;
+
+  @Column("text")
+  content: string;
+
+  @Column({ type: "char", length: 1, enum: ShowData })
+  showData: ShowData;
+
+  @UpdateDateColumn({ type: "date" })
+  createAt: Date;
+
+  @Column()
+  view: number;
+
+  @Column()
+  userId: string;
+
+  @Column()
+  userName: string;
+}
