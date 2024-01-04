74140f5ab8b73403bdf71d6403d7ab42597980a7
bobby <rlatnguscjsw@gmail.com>
2024-01-04 18:59:57 +0900

    Feat : Pbs Api
    - Create Pbs Api

--- src/pbs/pbs.controller.ts
+++ src/pbs/pbs.controller.ts
@@ -1,36 +1,66 @@
-import { Controller, Get, Post, Body, Param, Delete, Put } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete, Put, Query } from "@nestjs/common";
 /** Service */
-import { PbsService } from 'pbs/pbs.service';
+import { PbsService } from "pbs/pbs.service";
 /** Dto */
-import { CreatePbsDto } from 'pbs/dto/create-pbs.dto';
-import { UpdatePbsDto } from 'pbs/dto/update-pbs.dto';
+import { CreatePbsDto } from "pbs/dto/create-pbs.dto";
+import { UpdatePbsDto } from "pbs/dto/update-pbs.dto";
 
-@Controller('pbs')
+@Controller("pbs")
 export class PbsController {
   constructor(private readonly pbsService: PbsService) {}
 
+  /** Pbs Notice Api */
+  @Get()
+  async findAll(@Query("page") page: number, @Query("book") book: string) {
+    const pbsNotice = await this.pbsService.pbsFindAllNotice(page, book);
+
+    const count = await this.pbsService.pbsFindAllNoticeCount(book);
+
+    return { pbs: pbsNotice, length: count };
+  }
+
+  /** My Page Api */
+  @Get("mypage")
+  async findByUserId(@Query("page") page: number, @Query("book") book: string, @Query("uuid") uuid: string) {
+    const myPagePbsNotice = await this.pbsService.myPbsFindAllNotice(uuid, page, book);
+
+    const count = await this.pbsService.pbsFindMyPageNoticeCount(uuid, book)
+    
+    return { pbs: myPagePbsNotice, length: count };
+  }
+
+  /** View가 높은 순 10개 */
+  @Get("view")
+  findByView(@Query("book") book: string){
+    const view = this.pbsService.findByView(book);
+
+    return view;
+  }
+
+  /** 상세 페이지 */
+  @Get(":id")
+  findOne(@Param("id") id: string) {
+    const detailPbs = this.pbsService.pbsFindOne(+id);
+    return detailPbs;
+  }
+
+  /** Pbs 생성 */
   @Post()
   create(@Body() createPbsDto: CreatePbsDto) {
-    return this.pbsService.create(createPbsDto);
+    return this.pbsService.createPbs(createPbsDto);
   }
 
-  @Get()
-  findAll() {
-    return this.pbsService.findAll();
+  /** Pbs 수정 */
+  @Put(":id")
+  update(@Param("id") id: string, @Body() updatePbsDto: UpdatePbsDto) {
+    return this.pbsService.updatePbs(+id, updatePbsDto);
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.pbsService.findOne(+id);
-  }
+  /** 삭제 */
+  @Delete(":id")
+  remove(@Param("id") id: string) {
+    const deletePbs = this.pbsService.removePbs(+id);
 
-  @Put(':id')
-  update(@Param('id') id: string, @Body() updatePbsDto: UpdatePbsDto) {
-    return this.pbsService.update(+id, updatePbsDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.pbsService.remove(+id);
+    return deletePbs;
   }
 }
--- src/pbs/pbs.service.ts
+++ src/pbs/pbs.service.ts
@@ -1,11 +1,14 @@
-import { Repository } from 'typeorm';
-import { Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-/** Entity */
-import { PbsModel } from 'pbs/entities/pbs.entity';
+import { Injectable, NotFoundException } from "@nestjs/common";
+/** Type Orm */
+import { Like, Repository } from "typeorm";
+import { InjectRepository } from "@nestjs/typeorm";
 /** Dto */
-import { CreatePbsDto } from 'pbs/dto/create-pbs.dto';
-import { UpdatePbsDto } from 'pbs/dto/update-pbs.dto';
+import { CreatePbsDto } from "pbs/dto/create-pbs.dto";
+import { UpdatePbsDto } from "pbs/dto/update-pbs.dto";
+/** Entity */
+import { PbsModel } from "pbs/entities/pbs.entity";
+/** Enum */
+import { PbsShowData } from "pbs/const/pbs.const";
 
 @Injectable()
 export class PbsService {
@@ -14,7 +17,95 @@ export class PbsService {
     private readonly pbsRepository: Repository<PbsModel>,
   ) {}
 
-  async create(createPbsDto: CreatePbsDto) {
+  /** Pbs 게시판 리스트 */
+  async pbsFindAllNotice(page: number, book: string) {
+    const findAll = await this.pbsRepository.find({
+      /** Data 오름차순 */
+      order: {
+        id: "DESC",
+      },
+      where: {
+        showData: PbsShowData.PUBLIC,
+        book: Like(`%${book}%`)
+      },
+      take: 10,
+      skip: (page - 1) * 10
+    });
+
+    return findAll;
+  }
+
+  /** Pbs 게시판 개수 */
+  async pbsFindAllNoticeCount(book: string){
+    const noticeCount = await this.pbsRepository.count({
+      where: {
+        showData: PbsShowData.PUBLIC,
+        book: Like(`%${book}%`)
+      }
+    })
+
+    return noticeCount;
+  }
+
+  /** -------------------------------------------------- **/
+
+  /** My Page Pbs 리스트 */
+  async myPbsFindAllNotice(uuid: string, page: number, book: string) {
+    const findAll = await this.pbsRepository.find({
+      /** Data 오름차순 */
+      order: {
+        id: "DESC",
+      },
+      where: {
+        book: Like(`%${book}%`),
+        uuid: uuid,
+      },
+      take: 10,
+      skip: (page - 1) * 10
+    });
+
+    return findAll;
+  }
+
+  /** My Page Pbs 게시판 개수 */
+  async pbsFindMyPageNoticeCount(uuid: string, book: string){
+    const noticeCount = await this.pbsRepository.count({
+      where: {
+        uuid: uuid,
+        book: Like(`%${book}%`)
+      }
+    })
+
+    return noticeCount;
+  }
+
+  /** -------------------------------------------------- **/
+
+  /** Pbs 상세 페이지 */
+  async pbsFindOne(id: number) {
+    const found = await this.pbsRepository.findOne({
+      where: { id: id },
+    });
+
+    if (!found) {
+      throw new NotFoundException("해당 게시물을 찾을 수 없습니다.");
+    }
+
+    await this.pbsRepository.increment(
+      {
+        id: id,
+      },
+      "view",
+      1,
+    );
+
+    return found;
+  }
+
+  /** -------------------------------------------------- **/
+
+  /** Pbs 생성 */
+  async createPbs(createPbsDto: CreatePbsDto) {
     const createPbs = this.pbsRepository.create({ ...createPbsDto, view: 0 });
 
     const savePbs = await this.pbsRepository.save(createPbs);
@@ -22,19 +113,57 @@ export class PbsService {
     return savePbs;
   }
 
-  findAll() {
-    return `This action returns all pbs`;
+  /** Pbs 수정 */
+  async updatePbs(id: number, updatePbsDto: UpdatePbsDto) {
+    const foundPbs = this.pbsRepository.findOne({
+      where: { id: id } 
+    })
+
+    if (!foundPbs) {
+      throw new NotFoundException(`${id}는 없습니다.`);
+    }
+
+    const newPbs = {
+      id: id,
+      ...foundPbs,
+      ...updatePbsDto,
+    }
+
+    const updatePbs = this.pbsRepository.save(newPbs)
+
+    return updatePbs;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} pb`;
+  /** Pbs 제거 */
+  async removePbs(id: number) {
+    const found = await this.pbsRepository.findOne({
+      where: { id: id },
+    });
+
+    if (!found) {
+      throw new NotFoundException("해당 게시물을 찾을 수 없습니다.");
+    }
+
+    await this.pbsRepository.delete({
+      id: id,
+    });
+
+    return true;
   }
 
-  update(id: number, updatePbDto: UpdatePbsDto) {
-    return `This action updates a #${id} pb`;
-  }
+  /** View 가 높은 10개 데이터 */
+  async findByView(book: string){
+    const foundView = await this.pbsRepository.find({
+      order: {
+        view: "DESC",
+        id: "DESC"
+      },
+      where: {
+        book: Like(`%${book}%`)
+      },
+      take: 5
+    })
 
-  remove(id: number) {
-    return `This action removes a #${id} pb`;
+    return foundView;
   }
 }
